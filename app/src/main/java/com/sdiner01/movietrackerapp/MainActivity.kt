bb8947ecf9ccf5fae7dcf0fba1342ea181b011bb
SerkanDiner <123562186+SerkanDiner@users.noreply.github.com>
2025-08-04 16:57:49 +0100

    We successfully run our app to display greetings; Hello Serkan!

--- app/src/main/java/com/sdiner01/movietrackerapp/MainActivity.kt
+++ app/src/main/java/com/sdiner01/movietrackerapp/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             MovieTrackerAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Serkan",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
